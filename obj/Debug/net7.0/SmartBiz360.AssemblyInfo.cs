ad54aab5784a2c9d1187d6b756a03b92c0634ee6
maliktalha3111 <maliktalha3111@gmail.com>
2026-05-16 16:14:01 +0500

    Setup SQLite DB schema and EF Core migrations

--- obj/Debug/net7.0/SmartBiz360.AssemblyInfo.cs
+++ obj/Debug/net7.0/SmartBiz360.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartBiz360")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17553559da7089c4c81432ab00f5d9f30742ad13")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartBiz360")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartBiz360")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
