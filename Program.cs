918fcf51a701dc1e0a792bcae84a87248fccb3fd
ayeshairshad24 <241850@students.au.edu.pk>
2026-05-26 13:55:37 +0500

    Registered Campaign Service in the program.cs

--- Program.cs
+++ Program.cs
@@ -16,6 +16,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 builder.Services.AddScoped<AuthService>();
 builder.Services.AddScoped<SessionService>();
 builder.Services.AddScoped<CrmService>();
+builder.Services.AddScoped<CampaignService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
